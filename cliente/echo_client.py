8c6bf6b9ffdde38d2a884ba7453f0aad7ba4bf8a
Noe Fernandes Carvalho Pessoa <noefernandes@A101-10L.imd.local>
2019-10-07 18:20:38 -0300

    cliente enviando move

--- cliente/echo_client.py
+++ cliente/echo_client.py
@@ -1,6 +1,7 @@
 #reference: https://realpython.com/python-sockets/
 
 import socket
+import time
 
 HOST = '10.7.104.10'  # The server's hostname or IP address
 PORT = 65432        # The port used by the server
@@ -9,7 +10,11 @@ player_name = input("Digite seu nome: ")
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s: 
     s.connect((HOST, PORT))
-    s.sendall(player_name.encode("utf-8"))
+    s.sendall(("conn." + player_name).encode("utf-8"))
     data = s.recv(1024)
+
+    while(True):
+    	time.sleep(1)
+    	s.sendall(b"move.left")
 
 print('Received', repr(data))
