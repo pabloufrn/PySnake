a91314b67b6c401528dafac9256a95e6661df49a
Noe Fernandes Carvalho Pessoa <noefernandes@A202-13L.imd.local>
2019-10-09 16:39:17 -0300

    projeto antigo

--- cliente/echo_client.py
+++ cliente/echo_client.py
@@ -3,18 +3,20 @@
 import socket
 import time
 
-HOST = '10.7.104.10'  # The server's hostname or IP address
+HOST = '10.7.109.10'  # The server's hostname or IP address
 PORT = 65432        # The port used by the server
 
 player_name = input("Digite seu nome: ")
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s: 
     s.connect((HOST, PORT))
-    s.sendall(("conn." + player_name).encode("utf-8"))
+    s.sendall(("conn.0." + player_name).encode("utf-8"))
     data = s.recv(1024)
+
+    playerid = data
 
     while(True):
     	time.sleep(1)
-    	s.sendall(b"move.left")
+    	s.sendall(b"move." + playerid + b".left")
 
 print('Received', repr(data))
