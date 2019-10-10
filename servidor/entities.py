c975ea7fb7489e3f513d85c36477574d5c5b0774
Pablo Emanuell Lopes Targino <pabloemanuell@A202-14L.imd.local>
2019-10-10 13:03:07 -0300

    fazer

--- servidor/entities.py
+++ servidor/entities.py
@@ -1,8 +1,8 @@
 
 class Player:
-	def __init__(self, address):
+	def __init__(self, conn):
 		self.snake_size = 3
-		self.address = address
+		self.conn = conn
 		# self.set_position((0,0)) # posição aleatoria
 	def set_name(self, name):
 		self.name = name
--- servidor/gamemanager.py
+++ servidor/gamemanager.py
@@ -1,3 +1,6 @@
+import json
+
+
 class GameManager:
     #initializing
     def __init__(self):
@@ -10,53 +13,76 @@ class GameManager:
             print(e)
             self.sock.close()
             exit()
+
     def play(self):
         read_list = []
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.setblocking(0)
-    s.bind(('', port))
-    s.listen(5)
-    read_list.append(s)
-    players = []
-    while True :
-        readable, writeable, error = select.select(read_list,[],[])
-        for sock in readable:
-            if sock is s:
-                conn, info = sock.accept()
-                read_list.append(conn)
-                playerid = len(players)
-                players.append(Player(conn))
-                log_player_event(playerid, "um player foi conectado.")
-                send_command_to_player(playerid, "accept", str(playerid))
-            else:
-                data = sock.recv(1024)
-                if data:
-                    cmd, playerid, arg = data.decode("utf-8").split(".")
-                    playerid = int(playerid)
-                    self.process_command(cmd, playerid, arg)
-                    # todo: mandar eventos do jogo a cada intervalo
-                else:
-                    sock.close()
-                    read_list.remove(sock)
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.setblocking(0)
+            s.bind(('', port))
+            s.listen(5)
+            read_list.append(s)
+            players = []
+            while True :
+                readable, writeable, error = select.select(read_list,[],[])
+                for sock in readable:
+                    if sock is s:
+                        conn, info = sock.accept()
+                        read_list.append(conn)
+                        playerid = len(players)
+                        players.append(Player(conn))
+                        log_player_event(playerid, "um player foi conectado.")
+                        send__message__to_player(playerid, "accept", str(playerid))
+                    else:
+                        data = sock.recv(1024)
+                        if data:
+                            cmd, playerid, arg = data.decode("utf-8").split(".")
+                            playerid = int(playerid)
+                            self.process_command(cmd, playerid, arg)
+                            # todo: mandar eventos do jogo a cada intervalo
+                        else:
+                            sock.close()
+                            read_list.remove(sock)
 
-    def process_command(cmd, playerid, arg):
-        if(cmd == "conn"):
-            self.log_player_event(playerid, "Conectou-se ao servidor")
-            self.send_command_to_all(playerid, "conn", arg)
-        elif(cmd == "move"):
-            log_player_event(playerid, f"se moveu para {arg}")
-            self.send_command_to_all(playerid, "move", arg)
+    def process_message(cmd, message):
+        message = json.loads(json_message.decode("utf-8"))
+        if(message["event"] == "setup"):
+            player = self.players[message["playerid"]]
+            player.name = message["playername"]
+            # colocar em uma posição, definir o tamanho
 
     def log_player_event(self, playerid, message):
         player = self.players[playerid]
         print(f"{player.name} {message}.")
 
-    def send_command_to_all(self, playerid, command, arg):
+    def send__message__to_all(self, message):
         for player in self.players:
-            player.send_message(f"{command}.{playerid}.{arg}")
+            player.send_message(message)
 
-     def send_command_to_player(self, playerid, command, arg):
-        players[playerid].send_message(f"{command}.{playerid}.{arg}")
+     def send__message__to_player(self, message):
+        players[playerid].send_message(message)
 
     def gameOver(self):
         print "Game Over\n"
+
+
+''' Lista de coisas para fazer
+- Carregar configuração do servidor e enviar para o cliente.
+- Receber a configuração no cliente e exibir o jogo.
+- Processar um evento de movimento no servidor
+- Enviar evento de movimento
+    - receber o estado do jogo e atualizar
+'''
+
+''' exemplos de json
+Setup (enviador pelo cliente):
+{
+    "playername": 0
+}
+Setup (enviador pelo server):
+{
+    "playerid": 0,
+    "height": 20,
+    "width": 30 
+}
+
+'''
