fa8e7f9eb5468bb8425ba10de02f18d7bf9d4c43
Pablo Emanuell Lopes Targino <pabloemanuell@A204-03L.imd.local>
2019-10-10 14:58:42 -0300

    comecando a comunicação

--- servidor/gamemanager.py
+++ servidor/gamemanager.py
@@ -43,26 +43,56 @@ class GameManager:
                             sock.close()
                             read_list.remove(sock)
 
-    def process_message(cmd, message):
-        message = json.loads(json_message.decode("utf-8"))
-        if(message["event"] == "setup"):
-            player = self.players[message["playerid"]]
-            player.name = message["playername"]
-            # colocar em uma posição, definir o tamanho
+    def process_events(cmd, eventbytes):
+        event = json.loads(eventbytes.decode("utf-8"))
+        if(event["eventname"] == "setup"):
+            player = self.players[event["playerid"]]
+            player.name = event["playername"]
+            player.body = self.get_random_body()
 
-    def log_player_event(self, playerid, message):
-        player = self.players[playerid]
-        print(f"{player.name} {message}.")
+            #lembrete
+            '''para verificar se ela está morta, é necessário 
+            verificar se a proxima parte do corpo também colide'''
 
-    def send__message__to_all(self, message):
+
+    def get_player_body():
+        x = randint(1, self.width-1)
+        y = randint(1, self.height-1)
+        invalid = False
+        # todo: usar deque
+        body = []
+        for _ in range(0, (self.width-1)*(self.height-1)-1):
+            for player in self.player:
+                invalid = False
+                if((x, y) in player.body):
+                    if(x == self.width-2):
+                        x = 0
+                        if(y == self.height-2):
+                            y = 0
+                        else:
+                            y += 1
+                    else:
+                        x += 1
+                    invalid = True
+                    break
+            if(invalid == False):
+                break
+        if(invalid):
+            pass
+            # todo: não conseguiu achar uma posição aleatória válida
+        for _ in range(self.min_player_size):
+            body.append((x, y))
+        return body
+
+    def log_event(self, eventbytes):
+        print(eventbytes.decode("utf-8"))
+
+    def send_event_to_all(self, dictjson):
         for player in self.players:
-            player.send_message(message)
+            player.send_message(json.dumps(dictjson))
 
-     def send__message__to_player(self, message):
-        players[playerid].send_message(message)
-
-    def gameOver(self):
-        print "Game Over\n"
+     def send_event__to_player(self, dictjson):
+        players[playerid].send_message(json.dumps(dictjson))
 
 
 ''' Lista de coisas para fazer
@@ -76,10 +106,12 @@ class GameManager:
 ''' exemplos de json
 Setup (enviador pelo cliente):
 {
-    "playername": 0
+    "eventname": "setup"
+    "playername": "teste"
 }
 Setup (enviador pelo server):
 {
+    "eventname": "setup"
     "playerid": 0,
     "height": 20,
     "width": 30 
