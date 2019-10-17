4083336f5e491ce65182934c20efabf2394a1f9f
Pablo Emanuell Lopes Targino <pabloemanuell@A204-06L.imd.local>
2019-10-17 15:46:11 -0300

    nova branch

--- cliente/main.py
+++ cliente/main.py
@@ -7,33 +7,37 @@ import socket
 import json
  
 class cube(object):
-	rows = 20
-	w = 500
+
 	def __init__(self,start,dirnx=1,dirny=0,color=(255,0,0)):
 		self.pos = start
 		self.dirnx = 1
 		self.dirny = 0
 		self.color = color
- 
-	   
+		global width, height, rows, columns
+		self.w = width
+		self.h = height
+		self.rows = rows
+		self.columns = columns
+		
 	def move(self, dirnx, dirny):
 		self.dirnx = dirnx
 		self.dirny = dirny
 		self.pos = (self.pos[0] + self.dirnx, self.pos[1] + self.dirny)
  
 	def draw(self, surface, eyes=False):
-		dis = self.w // self.rows
+		disy = self.h // self.rows
+		disx = self.w // self.columns
 		i = self.pos[0]
 		j = self.pos[1]
  
-		pygame.draw.rect(surface, self.color, (i*dis+1,j*dis+1, dis-2, dis-2))
-		if eyes:
-			centre = dis//2
-			radius = 3
-			circleMiddle = (i*dis+centre-radius,j*dis+8)
-			circleMiddle2 = (i*dis + dis -radius*2, j*dis+8)
-			pygame.draw.circle(surface, (0,0,0), circleMiddle, radius)
-			pygame.draw.circle(surface, (0,0,0), circleMiddle2, radius)
+		pygame.draw.rect(surface, self.color, (i*disx+1,j*disy+1, disx-2, disy-2))
+		# if eyes:
+			# centre = dis//2
+			# radius = 3
+			# circleMiddle = (i*dis+centre-radius,j*dis+8)
+			# circleMiddle2 = (i*dis + dis -radius*2, j*dis+8)
+			# pygame.draw.circle(surface, (0,0,0), circleMiddle, radius)
+			# pygame.draw.circle(surface, (0,0,0), circleMiddle2, radius)
 	   
  
  
@@ -84,8 +88,8 @@ class snake(object):
 				if i == len(self.body)-1:
 					self.turns.pop(p)
 			else:
-				if c.dirnx == -1 and c.pos[0] <= 0: c.pos = (c.rows-1, c.pos[1])
-				elif c.dirnx == 1 and c.pos[0] >= c.rows-1: c.pos = (0,c.pos[1])
+				if c.dirnx == -1 and c.pos[0] <= 0: c.pos = (c.columns-1, c.pos[1])
+				elif c.dirnx == 1 and c.pos[0] >= c.columns-1: c.pos = (0,c.pos[1])
 				elif c.dirny == 1 and c.pos[1] >= c.rows-1: c.pos = (c.pos[0], 0)
 				elif c.dirny == -1 and c.pos[1] <= 0: c.pos = (c.pos[0],c.rows-1)
 				else: c.move(c.dirnx,c.dirny)
@@ -125,25 +129,25 @@ class snake(object):
 				c.draw(surface)
  
  
-def drawGrid(w, rows, surface):
-	sizeBtwn = w // rows
- 
+def drawGrid(w, h, rows, columns, surface):
+	sizeBtwny = w // rows
+	sizeBtwnx = h // columns
 	x = 0
 	y = 0
 	for l in range(rows):
-		x = x + sizeBtwn
-		y = y + sizeBtwn
- 
-		pygame.draw.line(surface, (255,255,255), (x,0),(x,w))
+		y = y + sizeBtwny
 		pygame.draw.line(surface, (255,255,255), (0,y),(w,y))
-	   
+
+	for c in range(columns):
+		x = x + sizeBtwnx
+		pygame.draw.line(surface, (255,255,255), (x,0),(x,h))
  
 def redrawWindow(surface):
-	global rows, width, s, snack
+	global rows, columns, width, height, s, snack
 	surface.fill((0,0,0))
 	s.draw(surface)
 	snack.draw(surface)
-	drawGrid(width,rows, surface)
+	drawGrid(width, height, rows, columns, surface)
 	pygame.display.update()
  
  
@@ -192,18 +196,20 @@ def main():
 		dictserver = json.loads(data.decode())
 		rows = dictserver["height"]
 		columns = dictserver["width"]
+		width = 800
+		height = 800
 
 		win = pygame.display.set_mode((width, height))
 		s = snake((255,0,0), (10,10))
 		snack = cube(randomSnack(rows, s), color=(0,255,0))
 		flag = True
+		print(width, height, rows, columns)
 	 
 		clock = pygame.time.Clock()
 	   
 		while flag:
 			pygame.time.delay(50)
 			clock.tick(10)
-			
 			
 			s.move()
 			if s.body[0].pos == snack.pos:
